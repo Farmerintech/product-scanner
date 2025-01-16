e2e4972da8f34e0bed3b7167ded09b82d0de41bf
farmerintech <farmerintech@gmail.com>
2025-01-16 20:12:04 +0100

    new

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -3,6 +3,6 @@ import { AddProduct, DeleteProduct, EditProduct, getAProduct, getProducts, ScanP
 import { AuthMiddleware } from "../middlewares/authMiddleware.js";
 
 const ProductRoute = Router();
-ProductRoute.post('/', AuthMiddleware, AddProduct).get('/', AuthMiddleware, getProducts).put('/:id', EditProduct)
+ProductRoute.post('/add_product', AuthMiddleware, AddProduct).get('/get_products', AuthMiddleware, getProducts).put('/:id', EditProduct)
 .delete('/:id', AuthMiddleware, DeleteProduct).post('/scan', AuthMiddleware, ScanProduct).get('/:id', AuthMiddleware, getAProduct)
 export default ProductRoute
